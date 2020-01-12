2a43bcfa739ec0ecbd0329027649fde62a5233f3
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-01-13 00:05:35 +0530

    sending data from server to clietn and vice-versa

---
+++ public/js/chat.js
@@ -0,0 +1,11 @@
+const socket = io();
+
+socket.on('message', message => {
+  console.log(message);
+});
+
+document.querySelector('#message-form').addEventListener('submit', event => {
+  event.preventDefault();
+  const message = event.target.elements.message.value;
+  socket.emit('sendMessage', message);
+});
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 const express= require("express");
+const http = require("http");
 const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicPathDirectory = path.join(__dirname, '../public');
 
 app.use(express.static(publicPathDirectory));
 
+io.on("connection", (socket) => {
+	console.log("new WebSocket connection.")
 
-app.listen(port, () => {
+	socket.emit("message", "Welcome to Chat App!");
+
+	socket.on("sendMessage", (message) => {
+		io.emit("message", message);
+	});
+})
+
+server.listen(port, () => {
 	console.log(`Server is up on port ${port}!`)
 })
